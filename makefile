51ed42cf35ddfde6457cc1691d6b9b2fae8074cb
Tyler Vanderhoef <tyler.j.vanderhoef@gmail.com>
2016-04-26 00:46:23 -0700

    Updates to file input. Now reads all .cats files in working directory. Probably should expand to using command parameter to specify root dir so as to not search full directory tree.

--- makefile
+++ makefile
@@ -1,13 +1,24 @@
 
 SRC = src
-INTERMEDIATE = build
+INTERMEDIATE = temp
 OUTDIR = bin
 LANGUAGE = lang_spec
 
-all: outdir princess
+IGNORE_WARNS = -Wno-deprecated-register -Wno-unneeded-internal-declaration -Wno-unused-function
+OPTS = -std=c++14 -Wall -pedantic $(IGNORE_WARNS)
+MODE = -D
+FILES = $(INTERMEDIATE)/parser.cpp $(INTERMEDIATE)/tokenizer.cpp $(SRC)/main.cpp
 
-princess: parser tokenizer
-	g++ -o $(OUTDIR)/$@  $(INTERMEDIATE)/parser.cpp $(INTERMEDIATE)/tokenizer.cpp $(SRC)/main.cpp
+all: outdir princess-debug clean-temp
+
+debug: princess-debug
+release: princess-release
+
+princess-release: parser tokenizer
+	g++ $(OPTS) $(FILES) $(MODE)RELEASE -o $(OUTDIR)/princess
+
+princess-debug: parser tokenizer
+	g++ $(OPTS) $(FILES) $(MODE)DEBUG -o $(OUTDIR)/princess
 
 tokenizer: $(LANGUAGE)/tokenizer.l
 	lex -o $(INTERMEDIATE)/$@.cpp $(LANGUAGE)/$@.l
@@ -19,6 +30,9 @@ outdir:
 	mkdir -p $(OUTDIR)
 	mkdir -p $(INTERMEDIATE)
 
-clean:
+clean: clean-temp
 	@rm -rf $(OUTDIR) $(INTERMEDIATE)
 	@rm -f *~ *#
+
+clean-temp:
+	@rm -rf $(INTERMEDIATE)
--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,111 @@
 #include <iostream>
 #include <errno.h>
 #include <stdio.h>
+#include <dirent.h>
+#include <vector>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-extern int yyparse();
 extern int yylex();
+extern void runOnTests();
 extern FILE* yyin;
 
 int file_num = 0;
 int file_num_max = 0;
-char** files;
+std::vector<std::string> *files;
 
-int yywrap() {
+extern "C" int yywrap() {
+  //close the currently open file.
   fclose(yyin);
+
+  //check if we are at the end of file list.
+  //if not, keep processing
   if (++file_num < file_num_max) {
-    if ((yyin = fopen(files[file_num], "r")) == 0) {
-      perror(files[file_num]);
+    std::cout << "Processing file: " << files->at(file_num) << std::endl;
+    //process next file
+
+    const char* filename = files->at(file_num).c_str();
+    if ((yyin = fopen(filename, "r")) == 0) {
+      //no go, so show error
+      perror(filename);
       exit(1);
     }
+
+    //open worked correctly
     return 0;
   } else {
+    //didn't open anything.
     return 1;
   }
 }
 
+void processFiles(const char* nextDir) {
+  DIR *dir;
+  struct dirent *ent;
+    
+  if ((dir = opendir (nextDir)) != NULL) {
+    /* print all the files and directories within directory */
+
+    std::vector<std::string> *fileList = new std::vector<std::string>();
+    
+    while ((ent = readdir (dir)) != NULL) {
+      std::string currentDir(nextDir);
+      auto current = currentDir + "/" + ent->d_name;
+      
+      if(ent->d_name[0] == '.') {
+	continue;
+      }
+
+      struct stat path_stat;
+      
+      if(stat(current.c_str(), &path_stat) != 0){
+	perror("Error stating file");
+	continue;
+      }
+      
+      int isDir = S_ISDIR(path_stat.st_mode);
+
+      
+      if(!isDir && strlen(ent->d_name) < 6){
+	continue; //cannot be a .cats file
+      }
+      
+      if(isDir != 0){
+	processFiles(current.c_str());
+      }
+      else {
+	if(current.substr(current.length() - 5, 5) == ".cats"){
+	  //our kind of file, so use this.
+	  fileList->push_back(ent->d_name);
+	  file_num_max++;	
+	}
+      }
+      
+    }
+
+    closedir (dir);
+
+    files = fileList;
+  } else {
+    /* could not open directory */
+    perror ("Couldn't open the test directory. Make sure you are in the root of the project, and not in the bin folder.");
+  }
+}
 
 int main (int argc, char **argv){
-  file_num_max = argc;
-  files = argv;
+
+  char* cwd = getcwd(NULL,0);
   
-  if (argc > 1) {
-    if ((yyin = fopen(argv[file_num], "r")) == 0 ) {
-      perror(argv[file_num]);
-      exit(1);
-    }
+  processFiles(cwd);
+  
+  //keep processing files. This will
+  //keep calling yywrap foreach arguement in argv.
+  if(file_num_max > 0){
+    while(yylex());
   }
 
-  while(yylex());
+  delete files;
   
   return 0;
 }
